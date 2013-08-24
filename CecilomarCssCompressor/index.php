86c0ab28269e61a24fe644978568b8887efe4096
Cecil O. Almonte <info@cecilomar.com>
2013-08-24 14:22:20 -0700

    Updated index.php

---
+++ CecilomarCssCompressor/index.php
@@ -0,0 +1,10 @@
+<?php
+  /**
+* @package    CssCompressor
+* @author     Cecil O. Almonte
+* @website    http://www.cecilomar.com/projects/csscompressor/
+* @copyright  Copyright (C) Cecilomar Design, Inc. 2013
+* @license    http://www.gnu.org/licenses/gpl.txt
+**/
+  
+  ?>
