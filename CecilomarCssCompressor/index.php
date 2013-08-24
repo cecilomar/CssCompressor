636f0f5186ddcdf5a49b4112ac9a13e32f27077f
Cecil O. Almonte <info@cecilomar.com>
2013-08-24 19:35:14 -0400

    Added function str_compress_css()
    The function uses preg_replace() to filter out  unwanted characters and
    whitespaces from a string. Source: http://pastebin.com/8Edw7cJh

--- CecilomarCssCompressor/index.php
+++ CecilomarCssCompressor/index.php
@@ -6,5 +6,11 @@
 * @copyright  Copyright (C) Cecilomar Design, Inc. 2013
 * @license    http://www.gnu.org/licenses/gpl.txt
 **/
-  
-  ?>
+
+function str_compress_css( $css, $overwrite_file = true ){
+	$code = preg_replace( '/;\s(?=})|\/\*.*\*\/|\s*/s', '', implode( file($css) ) );
+	$overwrite_file && file_put_contents( $css, $code );
+	return $code;
+}
+
+?>
